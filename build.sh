8cb71e296178dafd75bd7bb1e35811567c2c993f
Vo Xuan Nhi Tran <xuannhi2k6@gmail.com>
2026-08-29 12:45:31 +1000

    Clean up production build script

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
 
-python manage.py createsuperuser \
-    --noinput \
-    --username "$DJANGO_SUPERUSER_USERNAME" \
-    --email "$DJANGO_SUPERUSER_EMAIL" || true
+python manage.py collectstatic --no-input
+
+python manage.py migrate
