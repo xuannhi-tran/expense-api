2fa4c15098ff593da0da2ae2101db59ec8dc79f1
Vo Xuan Nhi Tran <xuannhi2k6@gmail.com>
2026-08-29 12:27:45 +1000

    Add production superuser setup

--- build.sh
+++ build.sh
@@ -3,3 +3,8 @@
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py createsuperuser \
+    --noinput \
+    --username "$DJANGO_SUPERUSER_USERNAME" \
+    --email "$DJANGO_SUPERUSER_EMAIL" || true
